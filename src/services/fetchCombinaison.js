cec29da1d817bcbbfe83e3cabffc3874a59531d6
Marvin LAUBIES <lovemarvin@live.fr>
2021-11-11 00:04:44 +0100

    Created service taking care of fetching cards and checking if the desired amout is in correct format

---
+++ src/services/fetchCombinaison.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import Joi from "joi";
+
+const apis = axios.create({
+  baseURL: "http://localhost:5000/",
+  timeout: 1000,
+  headers: { Authorization: process.env.REACT_APP_ACCESS_TOKEN },
+});
+
+export async function fetchCombinaisonOfCards(desiredAmount) {
+  try {
+    Joi.assert(desiredAmount, Joi.number().min(0).max(999).required());
+    const message = await apis.get("/shop/5/search-combination", {
+      params: {
+        amount: desiredAmount,
+      },
+    });
+    return [null, message.data];
+  } catch (err) {
+    console.log(err);
+    return [err];
+  }
+}
